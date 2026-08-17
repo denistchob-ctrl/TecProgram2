a566f0451f931fc5718572000115440e7d3b20f3
denistchob-ctrl <denis.tchob@gmail.com>
2026-08-17 17:47:32 -0300

    Teste 1

---
+++ teste.py
@@ -0,0 +1,25 @@
+#teste aula 1
+#git init
+#git add .
+#git commit -m "Inicio do Processo"
+#git branch -M main
+#git push -u origin main
+#
+
+#no git hub / em settings
+# Credentials
+# Fine-grained personal access tokens
+# Generate new token - limitar periodo e pasta de projetos
+
+#teste
+
+
+# echo "# TecProgram2" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/denistchob-ctrl/TecProgram2.git
+# git push -u origin main
+
+#teste
